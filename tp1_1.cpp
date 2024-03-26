6811c7ce432a994edf54f24e04a608083886776b
Leonardo <patoleo2013@gmail.com>
2024-03-26 13:42:08 -0300

    Realización de ejercicios de puntero

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,7 +1,13 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
 
 int main(){
-    cout << "Hola mundo" << endl;
+    printf("Hola mundo");
+    int num = 974, *punt;
+    punt = &num;
+    printf("\nEl contenido del puntero es: %d", *punt);
+    printf("\nLa dirección de memoria almacenada por el puntero es: %p", punt);
+    printf("\nLa dirección de memoria de la variable es: %p", &num);
+    printf("\nLa dirección de memoria del puntero es: %p", &punt);
+    printf("\nEl tamaño de memoria utilizado por la variable es: %d bytes", sizeof(num));
     return 0;
 }
